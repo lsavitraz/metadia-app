3d60bbf190a82ef5a9caf71eeaabd5c2f7167317
Leandro Savitraz <lsavitraz@gmail.com>
2026-02-23 17:24:54 -0300

    chore: limpar e organizar pubspec.yaml para versão 1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.metadia"
+    namespace = "br.com.appmetadia.metadia"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.metadia"
+        applicationId = "br.com.appmetadia.metadia"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
